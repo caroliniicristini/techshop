d5a1ff4587c34a8dee441ef4f107d36e22816b02
Carolini <67964919+caroliniicristini@users.noreply.github.com>
2025-01-03 18:37:50 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,120 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
 
-export default function Home() {
+function App() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <section className="header">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container">
+            <div
+              className="collapse navbar-collapse justify-content-center"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <img src="./logo.svg" alt="Logo" />
+                  </a>
+                </li>
+              </ul>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  id="search"
+                />
+                <button className="btn icon-search" type="submit">
+                  <FaSearch size={20} />
+                </button>
+              </form>
+
+              <ul className="navbar-nav w-100 justify-content-around align-items-center">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Sobre
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contato
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Blog
+                  </a>
+                </li>
+              </ul>
+              <ul className="navbar-nav">
+                <li className="nav-item d-flex">
+                  <a className="nav-link" href="#">
+                    <img src="./icon-heart.svg" alt="Coração" />
+                  </a>
+                  <a className="nav-link" href="#">
+                    <img src="./shopping-cart.svg" alt="Carrinho de compras" />
+                  </a>
+                  <a className="nav-link" href="#">
+                    <img src="./icon-profile.svg" alt="Perfil" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
+      <div className="categorias">
+        <ul class="list-unstyled d-flex justify-content-evenly h-100 align-items-center">
+          <li>
+            <img src="./icon-phones.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Phones</p>
+            </a>
+          </li>
+          <li>
+            <img src="./icon-computers.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Computers</p>
+            </a>
+          </li>
+          <li>
+            <img src="./icon-smart-watches.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Smart Watches</p>
+            </a>
+          </li>
+          <li>
+            <img src="./icon-cameras.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Cameras</p>
+            </a>
+          </li>
+          <li>
+            <img src="./icon-headphones.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Headphones</p>
+            </a>
+          </li>
+          <li>
+            <img src="./icon-gaming.svg" alt="Icon Phone" />
+            <a href="#">
+              <p>Gaming</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <img src="./hero.svg" class="w-100" />
+      <img src="./playstation.svg" class="w-100" />
+    </>
   );
 }
+
+export default App;
